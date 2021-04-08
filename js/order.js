6318293f7407075c83fd5b9e754f65abcf5934e7
emex4gman <emex4gman@gmail.com>
2021-04-08 13:22:59 -0400

    update

---
+++ js/order.js
@@ -0,0 +1,36 @@
+//let submitForm = document.getElementById('item');
+//console.log(submitForm);
+
+//submitForm.addEventListener('ADD TO CART', function(event){
+//event.preventDefault();
+//let itemType= document.getElementsByClassName('checkboxItems').value;
+//console.log(`${itemType}`)
+//document,getElementByClassName('checkboxItems').value="";
+
+//let myItem = document.createElement('li');
+//let newItem = document.createTextNode(`${itemType}`);
+//console.log(myItem);
+//myItem.appendChild(newItem);
+//document.getElementById('cartItem').appendChild(myItem);
+//});
+
+// let btnCart= document.getElementById('cart'); //assigning button element to a variable
+// console.log(btnCart);
+// btnCart.addEventListener('click', (event)=>{ // adding event listener to the button
+//     var inputTA= document.createElement('input'); //creating a new element “Text Area”
+//     var paymentMode= document.createElement('button'); //creating a new element “Button”
+//     paymentMode.setAttribute('name', 'submit'); //adding attributes 'name' to  Button.
+//     paymentMode.setAttribute('value','dfdfd'); //adding attributes 'value' to  Button.
+
+//     linebreak= document.createElement('br'); // creating a “New line “ element “br”
+//     console.log(paymentMode);
+//     inputTA.setAttribute('type','radio'); //adding attributes 'name' to  text area
+//     inputTA.setAttribute('name','payment'); //adding attributes 'name. to radio button
+//     let payment= document.getElementById('paymnt'); //selecting div paymnt
+//     payment.appendChild(inputTA);
+//     paymentMode.innerHTML="Place Order";
+//     inputTA.innerHTML="credi card";
+
+//     payment.appendChild(linebreak); //adding “br” to div
+//     payment.appendChild(paymentMode); //adding “button” to div
+// });
